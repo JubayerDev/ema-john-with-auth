f67a1ec59080802d89d8af6bee8d75d749585a80
JubayerDev <ajashes858@gmail.com>
2022-03-21 23:42:23 +0600

    all products information added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,9 +3,19 @@ import './Product.css';
 
 const Product = (props) => {
     const { img, name, seller, price, ratings } = props.product;
+    const { handleAddToCart } = props;
     return (
         <div className='product'>
             <img src={img} alt="productImg" />
+            <div className="product-info">
+                <p className="product-name">{name}</p>
+                <p className="product-price">Price: ${price}</p>
+                <p className='seller'><small>Manufacturer: {seller}</small></p>
+                <p className='ratings'><small>Rating: {ratings} star</small></p>
+            </div>
+            <button className="product-btn" onClick={()=>handleAddToCart(props.product)}>
+                <p>Add to cart</p>
+            </button>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,14 +8,18 @@ const Shop = () => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
-    },[])
+    }, [])
+    
+    const handleAddToCart = (product) => {
+        console.log(product);
+    }
     return (
         <div className='shop-container'>
             <div className="products-container">
-                {products.map(product => <Product key={product.id} product={product}></Product>)}
+                {products.map(product => <Product key={product.id} product={product} handleAddToCart={handleAddToCart}></Product>)}
             </div>
             <div className="cart-container">
-                <h4>Order Summary</h4>
+                <h5 className='order-summary'>Order Summary</h5>
             </div>
         </div>
     );
