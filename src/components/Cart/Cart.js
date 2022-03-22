da06c58717a649b7824f34dab9f10cd4d8a71707
JubayerDev <ajashes858@gmail.com>
2022-03-22 22:26:40 +0600

    all calculation added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import './Cart.css';
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
+    
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+
+    const tax = (total * 0.1).toFixed(2);
+    const grandTotal = total + shipping + parseFloat(tax);
+
     return (
-        <div>
-            <h5 className='order-summary'>Order Summary</h5>
+        <div className='cart'>
+            <h5>Order Summary</h5>
             <p>Selected Items: {cart.length}</p>
-            <p id="total-price">Total Price:</p>
-            <p>Total Shipping Charge:</p>
-            <p>Tax:</p>
-            <h6>Grand Total:</h6>
-            <button className='clear-cart-btn'>Clear Cart</button>
-            <button className='review-btn'><small>Review Order</small></button>
+            <p>Total Price: ${ total}</p>
+            <p>Total Shipping Charge: ${ shipping}</p>
+            <p>Tax: ${ tax}</p>
+            <h6>Grand Total: ${ grandTotal.toFixed(2)}</h6>
+            {/* <button>Clear Cart</button>
+            <button><small>Review Order</small></button> */}
         </div>
     );
 };
