a76bbf8e7f0d3dda2f5d0394f2efdd197b53f437
JubayerDev <ajashes858@gmail.com>
2022-03-31 17:05:30 +0600

    order details added

--- src/components/Reviewitem/ReviewItem.js
+++ src/components/Reviewitem/ReviewItem.js
@@ -2,10 +2,23 @@ import React from 'react';
 import './ReviewItem.css';
 
 const ReviewItem = ({product}) => {
-    const { name, price, shipping, quantity } = product;
+    const { name,img, price, shipping, quantity } = product;
     return (
-        <div>
-            <h2>Review</h2>
+        <div className='review-item'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="review-item-details-container">
+                <div className="review-item-details">
+                    <p className="product-name">{name}</p>
+                    <p>Price: <span className='orange-color'>${price}</span></p>
+                    <p><small>Shipping: { shipping}</small></p>
+                    <p><small>Quantity: { quantity}</small></p>
+                </div>
+                <div className="delete-container">
+                    <button>Delete</button>
+                </div>
+            </div>
         </div>
     );
 };
